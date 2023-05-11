45c65bb0929fc826830e48c3249ed1ddab4a655f
HuyLee15 <huylv@ahiho.com>
2023-05-12 01:47:04 +0700

    feat: update login func

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -113,12 +113,6 @@ const RegisterModal= () => {
         icon={FcGoogle}
         onClick={() => signIn('google')} 
       />
-      <Button 
-        outline 
-        label="Continue with Github"
-        icon={AiFillGithub}
-        onClick={() => signIn('github')}
-      />
       <div 
         className="
           text-neutral-500 
